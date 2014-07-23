bb069a0cad25bdd8f8e818038bb8ed172b41676c
hnavasiolau <henadz.navasiolau@gmail.com>
2014-07-23 21:31:45 +0300

    model: class TodoListItem

--- djangoproject/todolist/models.py
+++ djangoproject/todolist/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class TodoListItem(models.Model):
+    text = models.CharField(max_length=200)
+    isCompleted = models.BooleanField(default=False)
