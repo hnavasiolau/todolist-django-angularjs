6321bd99d1a99afaf8ee11d535e349384bda0be8
hnavasiolau <henadz.navasiolau@gmail.com>
2014-07-25 15:25:27 +0300

    refactoring - rename field

--- djangoproject/todolist/models.py
+++ djangoproject/todolist/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class TodoListItem(models.Model):
     text = models.CharField(max_length=200)
-    isCompleted = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
