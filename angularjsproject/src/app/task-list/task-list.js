36d7ff23112b36508a3bd3100379dba3a6a20520
hnavasiolau <henadz.navasiolau@gmail.com>
2014-07-25 17:25:17 +0300

    todo list with hardcoded data

--- angularjsproject/src/app/task-list/task-list.js
+++ angularjsproject/src/app/task-list/task-list.js
@@ -1,13 +1,37 @@
-angular.module( 'task-list', [
+var taskList = angular.module( 'task-list', [
     'ui.router'
-])
-    .config(function config( $stateProvider ) {
-        $stateProvider.state( 'task-list', {
-            url: '/task-list',
-            views: {
-                "main": {
-                    templateUrl: 'task-list/task-list.tpl.html'
-                }
+]);
+
+taskList.config(function config( $stateProvider ) {
+    $stateProvider.state( 'task-list', {
+        url: '/task-list',
+        views: {
+            "main": {
+                templateUrl: 'task-list/task-list.tpl.html'
             }
-        });
+        }
     });
+});
+
+taskList.controller('TaskListController', function(){
+    this.tasks = testTasks;
+});
+
+var testTasks = [
+    {
+        text: 'learn angular',
+        completed: false
+    },
+    {
+        text: 'learn django',
+        completed: false
+    },
+    {
+        text: 'set up django project',
+        completed: true
+    },
+    {
+        text: 'set up angular project',
+        completed: true
+    }
+];
