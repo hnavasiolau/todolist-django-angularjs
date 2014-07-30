3c13bae212e457d2b84c03da720457d1423a2910
hnavasiolau <henadz.navasiolau@gmail.com>
2014-07-30 18:18:40 +0300

    model enhanced - parent TodoList class added

--- djangoproject/todolist/models.py
+++ djangoproject/todolist/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-class TodoListItem(models.Model):
-    text = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+class TodoList(models.Model):
+	name = models.CharField(max_length=100)
+
+class TodoListItem(models.Model):	
+	todoList = models.ForeignKey(TodoList)
+	text = models.CharField(max_length=200)
+	completed = models.BooleanField(default=False)
