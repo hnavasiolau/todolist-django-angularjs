929205760c2f246de45970e70213a3c02dab979c
hnavasiolau <henadz.navasiolau@gmail.com>
2014-07-31 11:20:09 +0300

    - added TodoListViewSet - extended TodoListItemViewSet with filtering by todoList id capability - registered /todolistitem url in router

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 from todolist import views
 
 router = routers.DefaultRouter()
-router.register(r'todolist', views.TodoListItemViewSet)
+router.register(r'todolist', views.TodoListViewSet)
+router.register(r'todolistitem', views.TodoListItemViewSet)
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls))
--- djangoproject/todolist/views.py
+++ djangoproject/todolist/views.py
@@ -1,5 +1,12 @@
 from rest_framework import viewsets
+from todolist.models import TodoList
 from todolist.models import TodoListItem
+from rest_framework import filters
+
+class TodoListViewSet(viewsets.ModelViewSet):
+    model = TodoList
 
 class TodoListItemViewSet(viewsets.ModelViewSet):
     model = TodoListItem
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('todoList',)
